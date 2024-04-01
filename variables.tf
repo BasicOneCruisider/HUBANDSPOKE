6f900f9661bb2c39e396e18f9014b9db551d28e5
FrancisNtahimpera <Ntahimperafrancis@gmail.com>
2024-04-01 16:33:16 +0200

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "resource_group_name" {
+  description = "Nom du groupe de ressources"
+  type        = string
+  default     = "resoursouthuk89"
+}
+
+variable "location" {
+  description = "Emplacement du réseau"
+  type        = string
+  default     = "southuk"
+}
+
+variable "hub_subnet_cidr" {
+  description = "CIDR pour le sous-réseau du hub"
+  type        = string
+  default     = "10.0.0.0/24"
+}
+
+variable "spoke_subnet_cidr" {
+  description = "CIDR pour les sous-réseaux des spokes"
+  type        = string
+  default     = "10.1.0.0/24"
+}
